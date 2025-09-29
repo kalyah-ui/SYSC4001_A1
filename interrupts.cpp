0f292e0975e7c90f59970d6133253d0c0438cd93
kalyah.ui <basketball.mckesey@gmail.com>
2025-09-29 17:13:38 -0400

    Adding simulation and output to execution.txt

--- interrupts.cpp
+++ interrupts.cpp
@@ -5,8 +5,8 @@
  *
  */
 
-#include<interrupts.hpp>
-
+#include "interrupts.hpp"
+#include<cmath>
 int main(int argc, char** argv) {
 
     //vectors is a C++ std::vector of strings that contain the address of the ISR
@@ -19,19 +19,45 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
-
+    int time = 0;
+    int device_number = 0;
+    int num_activities = 0;
 
     /******************************************************************/
 
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
-        auto [activity, duration_intr] = parse_trace(trace);
+        if (trace.empty()) continue;
+            auto [activity, duration_intr] = parse_trace(trace);
 
-        /******************ADD YOUR SIMULATION CODE HERE*************************/
+            /******************ADD YOUR SIMULATION CODE HERE*************************/
+            if (activity == "END_IO") {
+            int device_number = duration_intr;
+            int duration = delays[device_number];
+            std::string isr_address = vectors[device_number];
 
+            num_activities = ceil(duration / 40);
 
+            execution += std::to_string(time) + ", 1, switch to kernel mode\n";
+            execution += std::to_string(time+1) + ", 10, context saved\n";
+            execution += std::to_string(time+11) + ", 1, find vector " + std::to_string(device_number+1) 
+            + " in memory position " + isr_address + "\n";
+            execution += std::to_string(time+12) + ", 1, obtain ISR adddress\n";
+            execution += std::to_string(time+13) + ", 24, call device driver\n";
 
+            for (int i = 0; i < num_activities; i++) {
+                execution += std::to_string(time+13 + i*40) + ", 40, ISR activity " 
+                + std::to_string(i+1) + " with device number: " + std::to_string(device_number + 1) + "\n";
+            }
+
+            execution += std::to_string(time+13 + num_activities*40) + ", 1, IRET\n";
+            execution += std::to_string(time+13 + num_activities*40 + 1) + ", " + std::to_string(duration)
+            + ", end of I/O " +std::to_string(device_number + 1) + ": interrupt\n";
+        }
+        else {
+            execution += std::to_string(time) + ", " + std::to_string(duration_intr) + ", " + activity + "\n";
+            time += duration_intr;
+        }
         /************************************************************************/
 
     }
